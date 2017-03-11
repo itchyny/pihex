d267fb88f88297e1dca2624598d4ee7a560e922b
itchyny <itchyny@hatena.ne.jp>
2017-03-11 13:43:43 +0900

    refactor main program formatting

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,9 @@ use pihex::*;
 
 fn main() {
     let place: u64 = env::args().nth(1).and_then(|x| x.parse().ok()).unwrap_or(0);
-    print!("{}: ", place);
+    print!("{}:", place);
     for i in 0..8 {
-        print!("{}", bbp::pihex(place + 4 * i));
-        if (i + 1) % 8 == 0 {
-            print!("\n")
-        } else {
-            print!(" ")
-        }
+        print!(" {}", bbp::pihex(place + 4 * i));
     }
+    print!("\n")
 }
