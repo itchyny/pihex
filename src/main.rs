071e661908e281ee0d54d9c77310449a4b2f7c30
itchyny <itchyny@hatena.ne.jp>
2017-03-14 08:34:49 +0900

    use clap-rs to parse arguments, support --formula={bbp,bellard} flag

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::env;
+#[macro_use]
+extern crate clap;
 
 extern crate pihex;
 use pihex::*;
 
 fn main() {
-    let place: u64 = env::args().nth(1).and_then(|x| x.parse().ok()).unwrap_or(0);
+    let matches = clap_app!(pihex =>
+        (version: "1.0")
+        (author: "itchyny <itchyny@hatena.ne.jp>")
+        (about: "Arbitrary place hexadecimal digits viewer of pi written in Rust")
+        (@arg FORMULA: --formula +takes_value "Formula to use (bbp or bellard)")
+        (@arg PLACE: "Place of digits to calculate (defaults to 0)"))
+        .get_matches();
+    let pihex: fn(u64) -> String = match matches.value_of("FORMULA") {
+        Some("bellard") => bellard::pihex,
+        _ => bbp::pihex,
+    };
+    let place: u64 = matches.value_of("PLACE").and_then(|x| x.parse().ok()).unwrap_or(0);
     print!("{}:", place);
     for i in 0..8 {
-        print!(" {}", bbp::pihex(place + 4 * i));
+        print!(" {}", pihex(place + 4 * i));
     }
     print!("\n")
 }
