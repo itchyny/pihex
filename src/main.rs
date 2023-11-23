dfcd5da492850b2d7e4a786ef4f85d24b622d8be
itchyny <itchyny@cybozu.co.jp>
2023-11-23 19:32:29 +0900

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{arg, Command};
 use pihex::*;
+use thiserror::Error;
 
 fn main() {
     if let Err(err) = run() {
@@ -8,7 +9,16 @@ fn main() {
     }
 }
 
-fn run() -> Result<(), String> {
+#[derive(Debug, Error)]
+enum Error {
+    #[error("unknown formula: {0}")]
+    Formula(String),
+
+    #[error("invalid place: {0}")]
+    Place(#[from] std::num::ParseIntError),
+}
+
+fn run() -> Result<(), Error> {
     let matches = Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -19,12 +29,12 @@ fn run() -> Result<(), String> {
     let pihex: fn(u64) -> String = match matches.get_one::<String>("formula").map(String::as_ref) {
         Some("bellard") => bellard::pihex,
         Some("bbp") | None => bbp::pihex,
-        Some(formula) => return Err(format!("unknown formula: {}", formula)),
+        Some(formula) => return Err(Error::Formula(formula.to_owned())),
     };
-    let place: u64 = matches
-        .get_one::<String>("PLACE")
-        .and_then(|x| x.parse().ok())
-        .unwrap_or(0);
+    let place: u64 = match matches.get_one::<String>("PLACE") {
+        Some(x) => x.parse()?,
+        None => 0,
+    };
     print!("{}:", place);
     for i in 0..8 {
         print!(" {}", pihex(place + 4 * i));
